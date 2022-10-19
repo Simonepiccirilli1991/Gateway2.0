5ce454f9dc158c463a9aa70d3b84f478b71a57d0
DESKTOP-P4RVHFF\proconsul <simone.piccirilli@proconsul-gropu.com>
2022-10-19 23:27:10 +0200

    junit mvc workante, e andiamo

--- src/test/java/com/gwsm0/controller/AppControllerTest.java
+++ src/test/java/com/gwsm0/controller/AppControllerTest.java
@@ -1,17 +1,21 @@
 package com.gwsm0.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.UnsupportedEncodingException;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.gwsm0.fragment.model.session.AppSessionRequest;
 import com.gwsm0.fragment.model.session.AppSessionResponse;
@@ -24,6 +28,7 @@ import com.gwsm0.rest.fragment.session.SecuretySessionFrag;
 import com.gwsm0.rest.service.ContextService;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AppControllerTest {
 
 	
@@ -35,6 +40,9 @@ public class AppControllerTest {
 	
 	@Autowired
 	private ContextService contexServ;
+	
+	@Autowired
+	protected MockMvc mvc;
 	
 	@Test
 	public void contextTestOk() {
@@ -68,5 +76,51 @@ public class AppControllerTest {
 		assertThat(contextCreate.getBody().getAppSecId()).isSameAs("sessionId");
 		assertThat(contextCreate.getStatusCode()).isSameAs(HttpStatus.OK);
 	}
+	
+	@Test
+	public void contextControllerTestOk() throws Exception {
+		
+		ContextRequest request = new ContextRequest();
+		request.setAbi("abi");
+		request.setAppName("appName");
+		request.setUsername("username");
+		
+		// sessione sicurezza
+		SessionSecResponse responseSicurezza = new SessionSecResponse();
+		responseSicurezza.setAbi("abi");
+		responseSicurezza.setBt("bt");
+		responseSicurezza.setScope("L2");
+		responseSicurezza.setSessionId("sessionId");
+		responseSicurezza.setSessionActive(true);
+		
+		// response di sessione applicativa
+		ResponseEntity<AppSessionResponse> appResponse = new ResponseEntity<AppSessionResponse>(new AppSessionResponse() ,HttpStatus.OK);
+		appResponse.getBody().setActive(true);
+		appResponse.getBody().setCodServizio("00");
+		appResponse.getBody().setAppSessionId("sessionId");
+		
+		Mockito.when(secSess.getSessionSec(ArgumentMatchers.any(SessionSecRequest.class))).thenReturn(responseSicurezza);
+		
+		Mockito.when(appSess.getSessionAppAdnError((ArgumentMatchers.any(AppSessionRequest.class)), ArgumentMatchers.anyString(), ArgumentMatchers.anyString())).thenReturn(appResponse);
+		
+		String mock =  mvc.perform(post("http://localhost:8084/app/context/create")
+				.contentType("application/json")
+				.header("abi", "abi")
+				.header("appname", "appname")
+//				.content(objectMapper.writeValueAsString(iRequest))
+				.content("{\r\n" + 
+						"    \"username\":\"ciao\",\r\n" + 
+						"    \"bt\":\"bt\"\r\n" + 
+						"}")
+				.characterEncoding("UTF-8"))
+				.andExpect(status().isOk())
+				.andReturn().getResponse()
+				.getContentAsString();
+		
+		System.out.println(mock);
+	}
+	
+	
+	
 }
 
